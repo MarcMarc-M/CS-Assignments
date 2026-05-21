12e28c6ab9ea8908f1f2d57c24bdd6c529e2c629
MarcMarc-M <galganamarc@gmail.com>
2026-05-21 08:54:46 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+while True:
+    user_input = input("Enter your age. (or tpye 'Exit' to quit): ")
+
+    if user_input == "Exit":
+        print("Thanks for checking the softwae out! Goodbye :D")
+        break
+ 
+    age = int(user_input)
+
+    if age < 18 or age > 65:
+        print("You're Eligible for the $5 Discount!")
+    else: 
+        print("No Discount Available :(")
